b3af8314a30af5d1b24b19a10d1783a120e927ef
Oluwapemzy <ojeksman2000@gmail.com>
2022-04-21 15:27:17 -0700

    changed name of argument to args

--- _printf.c
+++ _printf.c
@@ -12,12 +12,12 @@ int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
 	const char *p;
-	va_list arguments;
+	va_list args;
 	flags_t flags = {0, 0, 0};
 
 	register int count = 0;
 
-	va_start(arguments, format);
+	va_start(args, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
@@ -36,12 +36,12 @@ int _printf(const char *format, ...)
 				p++;
 			pfunc = get_print(*p);
 			count += (pfunc)
-				? pfunc(arguments, &flags)
+				? pfunc(args, &flags)
 				: _printf("%%%c", *p);
 		} else
 			count += _putchar(*p);
 	}
 	_putchar(-1);
-	va_end(arguments);
+	va_end(args);
 	return (count);
 }
